65455bf47c3c78cbef6da10d20146bad916206da
KOKUMUbooker <bookerochieng20@gmail.com>
2026-02-02 16:05:15 +0300

    Complete Two sum

---
+++ 1.TwoSum/twoSum.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] res = new int[2]; 
+
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = 0; j < nums.Length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        } 
+
+        return res;
+    }
+}
