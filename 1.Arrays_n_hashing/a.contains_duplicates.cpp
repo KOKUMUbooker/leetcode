566d7de43c62e7011ae8a7d5ffd3d3d05f7da0cc
KOKUMUbooker <bookerochieng20@gmail.com>
2026-08-29 18:19:05 +0300

    Complete contains duplicates

---
+++ 1.Arrays_n_hashing/a.contains_duplicates.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_map<int, int> existMap{};
+        for (const int num : nums) {
+            if (existMap.count(num) > 0)
+                return true;
+            else
+                existMap.try_emplace(num, 1);
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    Solution sln;
+    std::cout << std::boolalpha;
+
+    std::vector<int> test1 {1,2,3,4};
+    std::cout << sln.containsDuplicate(test1) << "\n";
+
+    std::vector<int> test2 {1,1,1,3,3,4,3,2,4,2};
+    std::cout << sln.containsDuplicate(test2) << "\n";
+
+    return EXIT_SUCCESS;
+}
